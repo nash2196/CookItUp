b5570dcdf31f50f8a0e331182d89620d36348213
nash2196 <nishant.nair11@hotmail.com>
2018-01-08 16:51:24 +0530

    Added logic to show recipe image, author and likes

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,41 @@
 var app = angular.module('MainApp',[]);
 
-app.controller('IngrController', function($http) {
+app.service('ingrService',function(){
+    var selectedIngr = [];
+
+    var addIngr = function(ingr){
+      selectedIngr.push(ingr);
+    };
+
+    var getIngr = function(){
+      return selectedIngr;
+    };
+
+    return {
+      addIngr: addIngr,
+      getIngr: getIngr
+    };
+});
+
+
+app.controller('IngrController', function($http, ingrService) {
       $http.get('/types').then((response) => {
       this.types = response.data;
       });
 
-      this.selectedIngr = [];
-
       this.checkIngr = function(option){
         var ingr = JSON.stringify(option);
-        var idx = this.selectedIngr.indexOf(ingr);
+        var idx = ingrService.getIngr().indexOf(ingr);
         if(idx>-1){
-          this.selectedIngr.splice(idx,1);
+          ingrService.getIngr().splice(idx,1);
         } else{
-          this.selectedIngr.push(ingr);
+          ingrService.addIngr(ingr);
         }
       };
 
       this.final = function(){
-        console.log(this.selectedIngr);
-      };
+        console.log(ingrService.getIngr());
+      }
 });
 
 
@@ -37,5 +53,64 @@ app.controller('MealController',function($http){
         selectedCuisine = JSON.stringify(c);
         console.log(selectedMeal,selectedCuisine);
       };
+});
+
+
+app.controller('RecipeController',function(ingrService){
+
+  this.recipes = recipes;
+  this.selectedRecipe = [];
+  var ingredients = ingrService.getIngr();
+  this.showRecipe = function(){
+    console.log(ingredients);
+    if(ingredients==[]){
+
+    }else {
+      loop1:
+      for (var i=0;i<ingredients.length;i++){
+        var ingr = ingredients[i];
+        // console.log("search param: ",ingr);
+      loop2:
+        for(var j=0;j<this.recipes.length;j++){
+      loop3:
+          for(var k=0;k<this.recipes[j].ingredients.length;k++){
+            var recipe_ingr = JSON.stringify(this.recipes[j].ingredients[k]);
+            // console.log("recipe ingr param: ",recipe_ingr);
+            if(ingr==recipe_ingr){
+              this.selectedRecipe.push(this.recipes[j]);
+              console.log(this.selectedRecipe[0]);
+              break loop1;
+            };
+          };
+        };
+      };
+
+    }
+  };
+
+
 
 });
+
+
+var recipes = [
+  {
+    name: "Onion Tomato",
+    ingredients: ["Onion","Tomato"],
+    images: ["recipe1-img1.jpeg","recipe1-img2.jpeg"],
+    meal_type: "Lunch",
+    cuisine_type: "Indian",
+    author: "Aditya Patel",
+    likes: 0
+  },
+
+  {
+    name: "Milk Banana",
+    ingredients: ["Milk","Banana"],
+    images: ["recipe2-img1.jpeg","recipe2-img2.jpeg"],
+    meal_type: "Breakfast",
+    cuisine_type: "Italian",
+    author: "Maitri Baria",
+    likes: 20
+  }
+];
