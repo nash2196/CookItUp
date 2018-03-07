de8b825bdea07b07045386ae00fa4c698c714c2d
Aditya PAtel <pateladitya151.ap@gmail.com>
2018-03-07 18:08:04 +0530

    added some vague gridfs code

--- server/server.js
+++ server/server.js
@@ -10,6 +10,9 @@ let mongoUtil=require('./mongoUtil');
 var path=require('path');
 var fs=require('fs');
 var mongoose = require('mongoose');
+var grid=require('gridfs-stream');
+var recipes=mongoose.model('recipes');
+
 
 app.use(express.static(path.join(__dirname, '/../client')));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,7 +40,7 @@ app.get('/meals', function(request,response){
 });
 
 app.get('/recipes', function(request,response){
-    mongoose.model('recipes').find(function(err,recipes){
+    recipes.find(function(err,recipes){
       response.send(recipes);
     });
 });
@@ -152,13 +155,106 @@ app.post('/upload/pic',multipartMiddleware, function(request,response){
 app.post('/upload/data',function(request,response){
 
   var formData = request.body;
-  console.log("Name: ",formData.recipeName);
-  console.log("ingredients: ",formData.ingredients);
-  console.log("meal_type: ",formData.meal_type," cuisine_type: ",formData.cuisine_type);
+  formData.save()
+  .then(console.log("recipe saved!"));
+  //.catch(console.log("failed to save recipe!"));
+  //console.log("Name: ",formData.recipeName);
+  //console.log("ingredients: ",formData.ingredients);
+  //console.log("meal_type: ",formData.meal_type," cuisine_type: ",formData.cuisine_type);
   response.send(formData.uuid);
 
 });
 
 app.listen(8181,function(){
   console.log("http://localhost:8181");
+});
+
+
+
+app.get('/images',function(request,response){
+  var photos=recipes.find({_id:request.body},{photos:true,_id:false});
+  console.log(recipe);
+  //var length=recipe.length;
+  var images;
+  for (var i = 0; i < photos.length; i++) {
+    gfs.files.find({ _id:photos[i] }).toArray(function (err, files) {
+   	    if(files.length===0){
+  			return res.status(400).send({
+  				message: 'File not found'
+  			});
+      }
+
+      //res.writeHead(200, {'Content-Type': files[0].contentType});
+
+  		var readstream = gfs.createReadStream({
+  			  //filename: files[0].filename
+  		});
+
+  	    readstream.on('data', function(data) {
+  	        images[i]=write(data);
+  	    });
+
+  	    readstream.on('end', function() {
+  	      //  res.end();
+  	    });
+
+  		readstream.on('error', function (err) {
+  		  console.log('An error occurred!', err);
+  		  throw err;
+  		});
+  	});
+  }
+  response.send(images);
+});
+
+app.post('/upload/pic',multipartMiddleware, function(request,response){
+    var uuid = null;
+    if(request.body.uuid===undefined){
+      //var file = request.files.file;
+      uuid = uuidv4();
+
+      //console.log(file.name);
+      //console.log(file.type);
+      //console.log(file.path);
+      //console.log("generated: ",uuid);
+      //response.send(uuid);
+    }else{
+    //var file = request.files.file;
+      uuid = request.body.uuid;
+      //console.log(file.name);
+      ///console.log(file.type);
+    //  console.log(file.path);
+      //console.log("from request: ",request.body.uuid);//
+    //  response.send(uuid);
+    }
+      //console.log("reached here!")
+    //var imgPath=path.join(__dirname,'/../client/img/recipe1-img1.jpeg');
+    var file=request.files.file.path;
+    console.log(file);
+    //conn.once('open',function(){
+      //console.log("inside");
+       grid.mongo=mongoose.mongo;
+       var gfs=grid(conn.db);
+       var writestream=gfs.createWriteStream();
+
+      fs.createReadStream(file).pipe(writestream);
+      writestream.on('close',function(){
+        console.log(" written to DB");
+      });
+
+    //
+    // fs.createReadStream(file).pipe(writestream);
+    // writestream.on('close',function(file){
+    //   console.log(file.filename+" written to DB");
+    // });
+    // writeStream.on('close', function() {
+    //   return response.status(200).send({
+    //     message: 'Success'
+    //   });
+    //  });
+
+
+    // writeStream.write(file.data);
+    // writeStream.end();
+    response.send(uuid);
 });
